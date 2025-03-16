98d6c0a13779212226abc089b0bc584d99ead9b5
wasim9695 <wasim10786akram@gmail.com>
2025-03-16 13:13:27 +0530

    sss

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: { domains: ['pplx-res.cloudinary.com','images.unsplash.com','chawkbazar.vercel.app','sakshigirri.com', 'manishmalhotra.in'], },
+  };
+  
+  
+  module.exports = nextConfig;
--- next.config.ts
+++
@@ -1,6 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = { images: { domains: ['pplx-res.cloudinary.com','images.unsplash.com','chawkbazar.vercel.app','sakshigirri.com', 'manishmalhotra.in'], }, };
-
-
-export default nextConfig;
